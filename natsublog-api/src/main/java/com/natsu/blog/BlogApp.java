a72891060331d469c170da1db3f47e0f923c3966
熊健琦 <1127695505@qq.com>
2022-10-06 15:50:19 +0800

    添加gitignore文件，并排除target文件

--- natsublog-api/src/main/java/com/natsu/blog/BlogApp.java
+++ natsublog-api/src/main/java/com/natsu/blog/BlogApp.java
@@ -3,7 +3,7 @@ package com.natsu.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.natsu.blog")
 public class BlogApp {
     public static void main(String[] args) {
         SpringApplication.run(BlogApp.class,args);
